a04bab22ebac2ef4f17ed4f050b7c06fa0167c74
Tim James <idoxlr8@gmail.com>
2013-04-17 10:27:23 -0500

    v1.1
    Changed to be compatible with tt-rss v1.7.9

--- init.php
+++ init.php
@@ -1,35 +1,20 @@
 <?php
 class article_toolbar extends Plugin {
 
-	private $link;
 	private $host;
 
 	function about() {
-		return array(1.0,
+		return array(1.1,
 			"Toolbar for easy access to feed functions",
 			"idoxlr8", false);
 	}
 
 	function init($host) {
-		$this->link = $host->get_link();
 		$this->host = $host;
 
 		$host->add_hook($host::HOOK_TOOLBAR_BUTTON, $this);
 	}
-	///Experimental get users name
-	function CDM_EXPANDED($link) {
 
-
-
-		if (!$user_id) {
-			$user_id = $_SESSION["uid"];
-		}
-			$result = db_query($link, "SELECT CDM_EXPANDED FROM ttrss_user_prefs WHERE owner_uid = $user_id");	
-			$value = db_fetch_result($result, 0, "CDM_EXPANDED");
-			return $value;
-	}		
-
-///
 	function get_js() {
 		return file_get_contents(dirname(__FILE__) . "/toolbar.js");
 	}
